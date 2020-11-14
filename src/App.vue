4264209208c9a7cc895b84f26f42bae627e743e7
Eder Sampaio <edersampaio@outlook.com.br>
2020-11-14 11:10:51 -0300

    Rename lib name

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@
       <router-view />
     </div>
 
-    <ColumnChart :xAxis="chart.xAxis" :series="chart.series" />
-
     <Footer />
   </div>
 </template>
@@ -17,36 +15,13 @@
 import Header from 'components/Header'
 import Menu from 'components/Menu'
 import Footer from 'components/Footer'
-import { ColumnChart } from '../dist/vuejs-highcharts.umd'
 
 export default {
   name: 'App',
   components: {
     Header,
     Menu,
-    Footer,
-    ColumnChart
-  },
-  data () {
-    return {
-      chart: {
-        xAxis: {
-          categories: [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]
-        },
-        series: [
-          {
-            name: 'Installation',
-            color: '#03bbd4',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-          },
-          {
-            name: 'Manufacturing',
-            color: '#f7a35c',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-          }
-        ]
-      }
-    }
+    Footer
   }
 }
 </script>
